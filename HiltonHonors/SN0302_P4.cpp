cf42a24865b014a3753ffef3c6c1bef992c6bac6
Noah Catchings <120297029+noahcatchings@users.noreply.github.com>
2024-04-18 21:42:36 -0500

    Update SN0302_P4.cpp

--- HiltonHonors/SN0302_P4.cpp
+++ HiltonHonors/SN0302_P4.cpp
@@ -1,8 +1,7 @@
 // Student Number: 0302
-// Course: CS155-03
-// Due Date: 4/17/24
+// Course Section - CS155 - 03
+// Due Date: x/x/xx
 // Description: 
-
 
 
 #include <iostream>
@@ -13,81 +12,108 @@ using namespace std;
 void displayHotels();
 int getHotelChoice();
 bool readRoomPrices(int hotelChoice, string& hotelName, 
-                    double& queen1Price, 
-                    double& queen2Price, double& king1Price, 
-                    double& king2Price, double& luxurySuite);
+	double& queen1Price, 
+	double& queen2Price, double& king1Price, 
+	double& king2Price, double&luxurySuite);
 void displayRoomPrices(const string& hotelName, double queen1Price, double queen2Price,
                        double king1Price, double king2Price, double luxurySuite);
 int getRoomChoice();
 double getRoomCost(int roomType, double queen1Price, double queen2Price,
                    double king1Price, double king2Price, double luxurySuite);
-int getNumRooms();
-int getNumNights();
-void addToOrder(int numNights, int numRooms, double roomCost, double& subtotal);
-double calculateTaxes(double subtotal);
-
-
+void printReceipt(const string& hotelName, int numRooms, int numNights,
+    double roomPrice, double subtotal, double taxFee, ofstream& outputFile);
 
 int main() {
-    int selection;
-    string hotelName;
+	int selection;
+	int roomNum;
+	int roomChoice;
+	string hotelName;
     double queen1Price, queen2Price, king1Price, king2Price, luxurySuite;
-    double roomCost;
-    double subtotal = 0.0;
-
-    do {
-        displayHotels();
-        selection = getHotelChoice();
-
-        if (selection != 0) {
-            if (!readRoomPrices(selection, hotelName, queen1Price, queen2Price, king1Price, king2Price, luxurySuite)) {
-                cout << "Failed to read room prices. Exiting..." << endl;
-                exit(1);
-            }
-
-            displayRoomPrices(hotelName, queen1Price, queen2Price, king1Price, king2Price, luxurySuite);
-
-            int roomChoice = getRoomChoice();
-            if (roomChoice != 0) {
-                int numRooms = getNumRooms();
-                int numNights = getNumNights();
-                roomCost = getRoomCost(roomChoice, queen1Price, queen2Price, king1Price, king2Price, luxurySuite);
-                addToOrder(numNights, numRooms, roomCost, subtotal);
-                calculateTaxes(subtotal);
-            }
-        }
-    } while (selection != 0);
-
-    cout << endl << "Goodbye!" << endl;
-    return 0;
+    
+    
+	displayHotels();
+	selection = getHotelChoice();
+	
+	if (!readRoomPrices(selection, hotelName, queen1Price, queen2Price, king1Price, king2Price, luxurySuite)) {
+        cout << "Failed to read room prices. Exiting..." << endl;
+        exit(1);
+    }
+    
+    displayRoomPrices(hotelName, queen1Price, queen2Price, king1Price, king2Price, luxurySuite);
+    
+    roomChoice = getRoomChoice();
+	do{
+		switch(roomChoice){
+			case 1: 
+				double getRoomCost(int roomType, double queen1Price, double queen2Price,
+                   double king1Price, double king2Price, double luxurySuite);
+				break;
+			case 2:
+				double getRoomCost(int roomType, double queen1Price, double queen2Price,
+                double king1Price, double king2Price, double luxurySuite);
+				break;
+			case 3: 
+				double getRoomCost(int roomType, double queen1Price, double queen2Price,
+                double king1Price, double king2Price, double luxurySuite);
+				break;
+			case 4:
+				double getRoomCost(int roomType, double queen1Price, double queen2Price,
+                double king1Price, double king2Price, double luxurySuite);
+				break;
+			case 5:
+				double getRoomCost(int roomType, double queen1Price, double queen2Price,
+                double king1Price, double king2Price, double luxurySuite);
+				break;
+			case 0:
+				cout << "Goodbye!" << endl;
+				exit(1);
+			default:
+				cout << "Invalid Choice. Please Try Again" << endl;
+				exit(1);
+			}
+		} while(roomChoice != 0);
+			
+		
+	
+	return 0;
 }
 
 void displayHotels(){
-    cout << "WELCOME TO THE HILTON HONORS BOOKING SYSTEM!" << endl;
-    cout << "Choose an option below:" << endl << endl;
-    cout << "1. Embassy Suites" << endl;
-    cout << "2. Hilton Garden" << endl;
-    cout << "3. DoubleTree" << endl;
-    cout << "0. End of the Day: Stop Processing" << endl << endl;
+	int choice; 
+	
+	cout << "WELCOME TO THE HILTON HONORS BOOKING SYSTEM!" << endl;
+	cout << "Choose an option below:" << endl << endl;
+	
+	cout << "1. Embassy Suites" << endl;
+	cout << "2. Hilton Garden" << endl;
+	cout << "3. DoubleTree" << endl;
+	cout << "0. End of the Day: Stop Processing" << endl;
+	
+	cout << endl;
 }
 
 int getHotelChoice() {
-    int choice;
-    cout << "Enter the number of your selected hotel: ";
-    cin >> choice;
-    while (choice < 0 || choice > 3){
-        cout << "INVALID CHOICE... PLEASE TRY AGAIN:" << endl;
-        cout << "Enter the number of your selected hotel: ";
-        cin >> choice;
-    }
-    return choice;
+	int choice;
+	
+	cout << "Enter the number of your selected hotel: ";
+	cin >> choice;
+	
+	while (choice < 0 || choice > 3){
+		cout << "INVALID CHOICE... PLEASE TRY AGAIN:" << endl;
+		cout << "Enter the number of your selected hotel: ";
+		cin >> choice;
+	}
+	
+	return choice;
 }
 
 bool readRoomPrices(int hotelChoice, string& hotelName, 
-                    double& queen1Price, 
-                    double& queen2Price, double& king1Price, 
-                    double& king2Price, double& luxurySuite) {
+    double& queen1Price, 
+    double& queen2Price, double& king1Price, 
+    double& king2Price, double& luxurySuite) {
+    
     string filename;
+    
     switch (hotelChoice) {
         case 1:
             filename = "Embassy.txt";
@@ -105,13 +131,17 @@ bool readRoomPrices(int hotelChoice, string& hotelName,
             cout << "Invalid hotel choice." << endl;
             return false;
     }
+    
     ifstream inFile(filename);
     if (inFile.fail()) {
         cout << "Error opening file: " << filename << endl;
         return false;
     }
+    
     inFile >> queen1Price >> queen2Price >> king1Price >> king2Price >> luxurySuite;
+    
     inFile.close();
+    
     return true;
 }
 
@@ -123,24 +153,28 @@ void displayRoomPrices(const string& hotelName, double queen1Price, double queen
     cout << "2 - 2 Queens: $" << queen2Price << endl;
     cout << "3 - 1 King: $" << king1Price << endl;
     cout << "4 - 2 Kings: $" << king2Price << endl;
-    cout << "5 - Luxury Suite: $" << luxurySuite << endl << endl;
+    cout << "5 - Luxury Suite: $" << luxurySuite << endl;
 }
 
 int getRoomChoice(){
-    int roomChoice;
-    cout << "Enter the number of your selected room: ";
-    cin >> roomChoice;
-    while (roomChoice < 0 || roomChoice > 5){
-        cout << "Error: Invalid Choice" << endl;
-        cout << "Enter the number of your selected room: ";
-        cin >> roomChoice;
-    }
-    return roomChoice;
+	int roomChoice;
+	
+	cout << "Enter the number of your selected room: ";
+	cin >> roomChoice;
+	
+	while (roomChoice < 0 || roomChoice > 5){
+		cout << "Error: Invalid Choice" << endl;
+		cout << "Enter the number of your selected room: ";
+		cin >> roomChoice;
+	}
+	
+	return roomChoice;
 }
 
 double getRoomCost(int roomType, double queen1Price, double queen2Price,
                    double king1Price, double king2Price, double luxurySuite) {
     double roomCost = 0.0;
+    
     switch(roomType) {
         case 1:
             roomCost = queen1Price;
@@ -160,49 +194,30 @@ double getRoomCost(int roomType, double queen1Price, double queen2Price,
         default:
             cout << "Invalid room type selected." << endl;
     }
+    
     return roomCost;
 }
-
-int getNumRooms(){
-    int rooms;
-    cout << "How many rooms will you need?: ";
-    cin >> rooms;
-    while (rooms < 1) {
-        cout << "Please enter a valid number of rooms" << endl;
-        cout << "How many rooms will you need?: ";
-        cin >> rooms;
+	
+void printReceipt(const string& hotelName, int numRooms, int numNights,
+                  double roomPrice, double subtotal, double taxFee, ofstream& outputFile) {
+                  	
+    double totalAmount = subtotal + taxFee;
+    ofstream outFile("OutNDC.txt"); 
+    if (!outFile.is_open()) {
+        cerr << "Error: Unable to open file " << outputFile << endl;
+        exit(1);
     }
-    return rooms;
+        
+    double totalAmount = subtotal + taxFee;
+    outFile << "Hotel Name: " << hotelName << endl;
+    outFile << "Number of Rooms: " << numRooms << endl;
+    outFile << "Number of Nights: " << numNights << endl;
+    outFile << "Room Price: $" << roomPrice << endl;
+    outFile << "Subtotal: $" << subtotal << endl;
+    outFile << "Tax Fee: $" << taxFee << endl;
+    outFile << "Total Amount: $" << totalAmount << endl;
+    outFile.close();
 }
-
-int getNumNights(){
-    int nights;
-    cout << "How many nights will you be staying?: ";
-    cin >> nights;
-    while (nights < 1) {
-        cout << "Please enter a valid number of nights" << endl; 
-        cout << "How many nights will you be staying?: ";
-        cin >> nights;
-    }
-    return nights;
-}
-
-void addToOrder(int numNights, int numRooms, 
-                double roomCost, double& subtotal) {
-    double reservation = numNights * numRooms * roomCost;
-    cout << "You have successfully booked the room(s) for " << numNights << " night(s)." << endl;
-    cout << "Total Cost: $" << reservation << endl << endl;
-    subtotal += reservation;
-}
-
-double calculateTaxes(double subtotal) {
-	double taxDollarAmount;
-	double salesTax = 0.04;
 	
-	taxDollarAmount = subtotal * salesTax;
 	
-	subtotal += taxDollarAmount;
-	
-	return subtotal;
-}
-
+			
